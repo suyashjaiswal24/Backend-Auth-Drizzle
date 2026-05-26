4e49fef86931c30d369898e76792338ff3a09417
Suyash Jaiswal <simple.sjaiswal@gmail.com>
2026-05-26 21:00:15 +0530

    Drizzle setup done

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+import {pgTable, uuid, varchar, text, boolean, timestamp} from "drizzle-orm/pg-core"
+
+export const users = pgTable('users', {
+    id: uuid('id').primaryKey().defaultRandom(),
+
+    firstName: varchar('first_name', { length: 45 }).notNull(),
+    lastName: varchar('last_name', { length: 45 }),
+
+    email: varchar('email', { length: 322 }).notNull().unique(),
+    emailVerified: boolean('email_verified').notNull().default(false),
+
+    password: varchar('password', { length: 66 }),
+    salt: text('salt'),
+
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+})
+
+
+// ORM - JS(camelCase) <-> DB(snake_case) mapping
